12d30aaee51f9f1114c3c87a18a5f11d373a3b46
yass8n <yass8n@yahoo.com>
2015-04-29 01:22:05 -0700

    created ruby script to show all the minimum values from the runs

---
+++ p4/parse_output.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+#Simple program to parse through output.txt and print only the timing data with 
+#arrows around the minimum value
+file = File.new("output/output.txt", "r")
+$i = 0;
+$arr = []
+while (line = file.gets)
+	$i = 0 if $i == 5
+	if $arr.length == 5 then
+		$min = $arr.min
+		$min = $min.to_s
+		$arr.map! do |num|
+			if num.to_s == $min then
+			     "-> " + num.to_s + " <-" 
+		    else 
+		    	num.to_s
+		    end
+		end
+		$arr.each do |num|
+			puts num
+		end
+        $arr.clear
+	end
+	puts "\n" if $i == 0
+	if line.include? "Completed in " then
+		line.gsub! "Completed in " , ""
+		line.gsub! "\n", ""
+		line.gsub! "sec", ""
+		$arr.push(line.to_f)
+		$i = $i + 1
+	end
+end
